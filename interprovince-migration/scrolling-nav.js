1c886e02e7502a48d69ca3aaf5d4e05a50c0ee70
Ilya Ilyankou <ilya.ilyankou@trincoll.edu>
2018-03-03 23:36:33 -0500

    Assignment 1 upload

---
+++ interprovince-migration/scrolling-nav.js
@@ -0,0 +1,32 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  const OFFSET = 50;
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - OFFSET
+        }, 1000, "easeInOutExpo");
+        console.log(target)
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: OFFSET + 10
+  });
+
+})(jQuery); // End of use strict
